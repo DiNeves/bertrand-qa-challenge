5bec4cfdc5580d1178de9889a629356c34e6c901
DiNeves <dms.neves@gmail.com>
2026-06-29 09:41:56 +0100

    Change  workers properties on playwright config file

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import os from 'os';
 
 /**
  * Read environment variables from file.
@@ -20,7 +21,10 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI 
+    ? Math.min(4, os.cpus().length) // cap at 4 in CI
+    : undefined, // default locally
+  /*? 2 : 0, */
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
