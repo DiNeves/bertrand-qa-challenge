f99e5d5f2210ed6c94e1a5c9e0245d821d02d699
DiNeves <dms.neves@gmail.com>
2026-06-29 23:01:18 +0100

    Create scenario 2 for 4 browsers

--- tests/specs/scenario2.spec.js
+++ tests/specs/scenario2.spec.js
@@ -23,10 +23,32 @@ test.describe('Test Suite Scenario 2', () => {
         const productListPage = new ProductListPage(page);
         const productDetailsPage = new ProductDetailsPage(page);
 
-        await test.step('Search for book 1984', async () => {
+        await test.step('Search for Book 1984', async () => {
             await headerPage.fillSearchBar(SCENARIO2.expectedFirstBook);
             const productsList = productListPage.getProductsList();
             await productListPage.selectProductFromTable(SCENARIO2.productSelectedIdPosition);
+        });
+
+        const firstBookAuthor = await productDetailsPage.getAuthor();
+
+        await test.step('Validate Author Name From First Book', async () => {    
+            expect(firstBookAuthor).toBe(SCENARIO2.expectedBookAuthor);
+        });
+
+        await test.step('Search for Book A Quinta dos Animais', async () => {
+            await headerPage.fillSearchBar(SCENARIO2.expectedSecondBook);
+            const productsList = productListPage.getProductsList();
+            await productListPage.selectProductFromTable(SCENARIO2.productSelectedIdPosition);
+        });
+
+        const secondBookAuthor = await productDetailsPage.getAuthor();
+
+        await test.step('Validate Author Name From Second Book', async () => {    
+            expect(secondBookAuthor).toBe(SCENARIO2.expectedBookAuthor);
+        });
+
+        await test.step('Validate Books Have Same Author Name', async () => {
+            expect(firstBookAuthor).toBe(secondBookAuthor);
         });
 
     });
